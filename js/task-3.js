3e6ee64dd062b7a83b40e0e673e41290835c4f20
schurrmurr <anastasyaraay@gmail.com>
2024-01-13 19:30:27 +0200

    updated

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
 'use strict';
 function getElementWidth(content, padding, border){
-    const message = Number.parseFloat(content) + (Number.parseFloat(padding) * 2 + Number.parseFloat(border)) * 2;
-    return message;
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    return totalWidth;
 }
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
